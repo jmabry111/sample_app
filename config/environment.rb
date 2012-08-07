07427e89294be5de1d05143f870d0f3c1fe5994f
Jason Mabry <jmabry@mymabry.com>
2012-08-07 16:31:01 -0400

    finish user signup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+#Speed up tests by lowering BCrypts's cost function
+require 'bcrypt'
+silence_warnings do
+  BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+end
+
