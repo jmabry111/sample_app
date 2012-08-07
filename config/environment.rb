3d4ad473ee5275e6361143efef009162fa641fb2
Jason Mabry <jmabry@mymabry.com>
2012-08-07 16:32:51 -0400

    add ssl in production

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,16 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
+SampleApp::Application.configure do
+  # Load the rails application
+  require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
-SampleApp::Application.initialize!
+  # Initialize the rails application
+  SampleApp::Application.initialize!
 
-#Speed up tests by lowering BCrypts's cost function
-require 'bcrypt'
-silence_warnings do
-  BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  #Speed up tests by lowering BCrypts's cost function
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
+  
+  config.force_ssl = true
+
 end
-
