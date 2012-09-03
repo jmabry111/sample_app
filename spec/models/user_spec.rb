288df12a871453c7161165b49979b865ba39816e
Jason Mabry <jmabry@mymabry.com>
2012-09-03 17:57:20 -0400

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -110,4 +110,19 @@ describe User do
       it { should be_invalid }
     end
   end
+  
+  it {should respond_to(:admin)}
+  it {should respond_to(:authenticate)}
+  
+  it {should be_valid}
+  it {should_not be_admin}
+  
+  describe "with admin attribute set to true" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    
+    it {should be_admin}
+  end
 end
